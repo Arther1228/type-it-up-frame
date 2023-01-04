d884a4332d4609fbf7e932f98af4bbaabc25a696
arther606 <>
2023-01-04 17:20:07 +0800

    springboot starter demo

--- springboot-demo/src/test/java/com/yang/springboot/JavaDemoProjectApplicationTests.java
+++ springboot-demo/src/test/java/com/yang/springboot/JavaDemoProjectApplicationTests.java
@@ -1,7 +1,9 @@
 package com.yang.springboot;
 
-import org.junit.Test;
+import com.yang.springboot.starter.demo.config.MyStarterConfig;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class JavaDemoProjectApplicationTests {
 
+    @Autowired
+    private MyStarterConfig myStarterConfig;
+
     @Test
-    public void contextLoads() {
+    public void testMyStarter(){
+        String name = myStarterConfig.getName();
+        System.out.println(name);
     }
 
 }
