c7af551fc971576c6b64c2b9ce4bb00ff7dde359
arther606 <396986654@qq.com>
2024-01-24 14:10:02 +0800

    aop update

--- springboot-demo/src/main/java/com/yang/springboot/demo/aop/LoggingAspect.java
+++ springboot-demo/src/main/java/com/yang/springboot/demo/aop/LoggingAspect.java
@@ -3,18 +3,27 @@ package com.yang.springboot.demo.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    @Before("execution(* com.yang.springboot.demo.aop.KkCustomsService.saveKkCustoms(..)) && args(model, method)")
-    public void logBeforeMethodExecution(JoinPoint joinPoint, String model, String method) {
+    @Before("execution(* com.yang.springboot.demo.aop.KkCustomsService.saveKkCustoms(..))")
+    public void logBeforeMethodExecution(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getTarget().getClass().getName();
 
+        // 获取调用方的类名和方法名
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        String callerClassName = method.getDeclaringClass().getName();
+        String callerMethodName = method.getName();
+
         System.out.println("Executing method " + methodName + " in class " + className);
-        System.out.println("Method parameters: model=" + model + ", method=" + method);
+        System.out.println("Called by method " + callerMethodName + " in class " + callerClassName);
     }
 }
